38d3f95b6601392563a74a0172c0c208d37165ed
Laurent Sarrazin <laurent@sarrazin.io>
2014-07-10 17:17:31 +0200

    Unit tests setup

---
+++ tests/LaurentSarrazin/Myfox/Tests/MyfoxTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace LaurentSarrazin\Myfox\Tests;
+
+use LaurentSarrazin\Myfox\Myfox;
+
+class MyfoxTest extends \PHPUnit_Framework_Testcase
+{
+    public function test_()
+    {
+        $myfox_client = new Myfox('token');
+    }
+} 
